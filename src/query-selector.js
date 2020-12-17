d8ea9506d51ca5096e8fa5bf8fc1c4a4e383e1a1
Sergey Kuznetsov <amatsukine@gmail.com>
2020-12-17 10:00:15 +0300

    querySelector() will throw an error if root is null

--- src/query-selector.js
+++ src/query-selector.js
@@ -12,11 +12,18 @@
  *
  * @returns {HTMLElement | null}
  * HTML element if finded, `null` otherwise.
+ *
+ * @throws
+ * Throws an error if `root == null`.
  */
 export function querySelector(
     selectors,
     root = document
 ) {
+    if (root == null) {
+        throw new Error('Passed root element does not exists');
+    }
+
     let value = null;
 
     for (const selector of selectors) {
